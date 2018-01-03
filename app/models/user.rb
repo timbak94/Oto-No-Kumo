3d065501528a7dc4d647dc6a6ece4abaf2097ec5
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2018-01-03 15:33:02 -0500

    attempt

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   validates :username, :password_digest, :session_token, :avatar_url, presence: true
   validates :username, uniqueness: true
   validates :password, length: { minimum: 6 }, allow_nil: true
+
   after_initialize :ensure_session_token, :ensure_avatar
 
   attr_reader :password, :avatar_url
