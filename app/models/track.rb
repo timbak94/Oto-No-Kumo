833b35ad93634692fe683f6a6896ea67bb79e5ff
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2018-01-11 09:35:20 -0500

    paperclip

--- app/models/track.rb
+++ app/models/track.rb
@@ -15,8 +15,7 @@ class Track < ApplicationRecord
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
   has_attached_file :track_url
- #  validates_attachment_content_type :track_url,
- # :content_type => [ 'audio/mpeg', 'audio/x-mpeg', 'audio/mp3', 'audio/x-mp3', 'audio/mpeg3', 'audio/x-mpeg3', 'audio/mpg', 'audio/x-mpg', 'audio/x-mpegaudio', 'application/octet-stream' ]
+  validates_attachment_content_type :track_url, :content_type => [ 'audio/mpeg', 'audio/x-mpeg', 'audio/mp3', 'audio/x-mp3', 'audio/mpeg3', 'audio/x-mpeg3', 'audio/mpg', 'audio/x-mpg', 'audio/x-mpegaudio', 'application/octet-stream' ]
 
   validates :title, :description, :author_id, :genre, presence: true
   validates :genre, inclusion: { in: GENRES }
@@ -31,8 +30,29 @@ class Track < ApplicationRecord
     foreign_key: :track_id,
     primary_key: :id
 
+    module Paperclip
+      class MediaTypeSpoofDetector
+
+        def supplied_content_type
+          +      if @content_type == "application/octet-stream"
+            +        @content_type = calculated_content_type
+            +      else
+            @content_type
+            +      end
+          end
+        end
+      end
 end
 
-
 module Paperclip
+  class MediaTypeSpoofDetector
+
+    def supplied_content_type
++      if @content_type == "application/octet-stream"
++        @content_type = calculated_content_type
++      else
+        @content_type
++      end
+    end
+  end
 end
